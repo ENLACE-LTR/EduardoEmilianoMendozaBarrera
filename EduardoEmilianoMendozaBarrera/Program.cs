c47d13f8837882b8dd362a15a806affb9df99182
ENLACE-LTR <ninjapuffle10@gmail.com>
2023-11-08 03:02:10 -0600

    Examen segundo parcial

--- EduardoEmilianoMendozaBarrera/Program.cs
+++ EduardoEmilianoMendozaBarrera/Program.cs
@@ -1,2 +1,12 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿/*
+a. ¿Qué hace el comando git reset --soft HEAD~1?
+Un soft reset solo restablecerá el historial de confirmaciones. De manera predeterminada, git reset se invoca con HEAD como la confirmación objetivo
+b. ¿Qué hace el comando git restore --staged . ?
+Restaura el estado de un archivo a una revisión específica de git.
+c. ¿Qué hace el comando git restore . ?
+Deshacer modificaciones hechas a archivos. 
+d. ¿Qué hace el comando git clean -f ?
+Eliminar archivos no deseados de tu directorio de trabajo. 
+e. ¿Qué hace el comando git merge --abort ?
+Intenta volver a su estado antes de ejecutar la fusión.
+*/
